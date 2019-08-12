586607955fbfb999bbb93f205b8d1b08da235d93
wahyu_mk <wahyu.mk19@gmail.com>
2019-08-12 22:01:09 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h2>Coba GitHub</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum nam maiores totam, laudantium veniam sit ad architecto id quidem, asperiores quae odit recusandae necessitatibus, eveniet laborum pariatur illo neque aperiam.</p>
 </body>
 </html>
